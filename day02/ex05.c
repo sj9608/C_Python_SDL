ffffb7ade635edf519df3d42b555bbe515cbc2d0
lsj <sneak7581@wku.ac.kr>
2020-07-28 14:17:06 +0900

    day02 ex05 update

---
+++ day02/ex05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char opr;
+    int a = 0, b = 0, Result = 0;
+
+    printf("input a op b : ");
+    scanf("%d %c %d",&a, &opr, &b);
+
+    if (opr == '+')
+    {
+        printf("Result : %d + %d = %d", a, b, a + b);
+    }
+    else if (opr == '-')
+    {
+        printf("Result : %d - %d = %d", a, b, a - b);
+    }
+    else if (opr == '*')
+    {
+        printf("Result : %d * %d = %d", a, b, a * b);
+    }
+    else if (opr == '/')
+    {
+        printf("Result : %d / %d = %d", a, b, a / b);
+    }
+        else if (opr == '%')
+    {
+        printf("Result : %d %% %d = %d", a, b, a / b);
+    }
+
+    return 0;
+}
