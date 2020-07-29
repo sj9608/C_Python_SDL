bb6d80c0df6a26f01061d760ad4064f18614371a
lsj <sneak7581@wku.ac.kr>
2020-07-29 10:11:36 +0900

    third commit

--- day02/ex06.py
+++ day02/ex06.py
@@ -1,10 +1,17 @@
 from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+import turtle as t
+
+t.speed(10)
+t.pencolor("Red")
+
+for i in range(9):
+       t.circle(60, steps = 10)
+       t.lt(40)
+
+t.pencolor("Blue")
+
+for j in range(6):
+       t.lt(60)
+       t.circle(60, steps = 5)
+
+input()
