e8737d7200d71dbd16815cf0ca4b1bbb220f1ce4
lsj <sneak7581@wku.ac.kr>
2020-07-29 16:31:30 +0900

    ex06.c modified

--- day03/ex06.c
+++ day03/ex06.c
@@ -7,6 +7,7 @@ int main()
     int c = 012; //8진수 표현은 앞에 '0' 만 붙이면 된다.
     int d = 0b1111; //2진수 표현 (c99 문법)
     char *strMsg = "Hello World!"; //문자열 상수 저장
+    
 
     printf("%f %d %d %d %s \n", a, b, c, d,strMsg);
 
