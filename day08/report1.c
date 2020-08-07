26fa6b8d6ef2c9fbc9dc40ca49e1e814ffe6566b
KINOTZ <sneak7581@wku.ac.kr>
2020-08-07 09:36:00 +0900

    complete report1.c

--- day08/report1.c
+++ day08/report1.c
@@ -11,33 +11,56 @@ int main(int argc, char *argv[])
     }
 
     /* 4 x 4 size star*/
-    for(int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        for(int j =0; j < 4; j++)
+        for (int j = 0; j < 4; j++)
         {
-            printf("%c",'*');
+            printf("%c", '*');
         }
         printf("\n");
     }
 
     /* stair star */
-    for(int i =0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for(int j = 0; j < i; j++)
+        for (int j = 0; j < i; j++)
         {
-                printf("%c",'*');
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    /* diamond */
+    int _stair = 3;
+    for (int i = 0; i < _stair; i++)
+    {
+        for (int j = _stair - 1; j > i; j--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
         }
         printf("\n");
     }
 
-    for(int i = 0; i < 5; i++)
+    for (int i = 1; i < _stair; i++)
     {
-        for(int j = i+1 ; j < i ; j++)
+        for (int j = 0; j < i; j++)
         {
-            printf("%c",'*');
+            printf(" ");
+        }
+
+        for (int j = 2 * _stair - 1; j > 2 * i; j--)
+        {
+            printf("*");
         }
         printf("\n");
     }
+
     SDL_Quit();
     return 0;
 }
