24f1a2f3d1eeb5f890df770a2fcba2ce8174b997
KINOTZ <sneak7581@wku.ac.kr>
2020-08-07 17:33:59 +0900

    engine folder upload

---
+++ engine/parser.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_TOKEN_SIZE 32
+
+int doTokenize(char *szBuf, char szBufToken[8][MAX_TOKEN_SIZE])
+{
+  //change 10
+  //[change,10]
+  char *szpTemp;
+  char *pNextToken = NULL;
+  const char *pszDelimiter = " ";
+  szpTemp = strtok(szBuf, pszDelimiter);
+
+  int _nTokenIndex = 0;
+  while (szpTemp != NULL)
+  {
+    strcpy(szBufToken[_nTokenIndex],szpTemp);
+    _nTokenIndex++;
+    szpTemp = strtok(NULL, pszDelimiter);
+  }
+  return _nTokenIndex;
+} 
---
+++ engine/tile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+void putTile(SDL_Renderer *pRenderer, SDL_Texture *pTex,Uint16 _x,Uint16 _y, Uint16 _index)
+{
+  SDL_Rect _tmpDstRt;
+  _tmpDstRt.x = _x *32;
+  _tmpDstRt.y = _y *32;
+  _tmpDstRt.w = 32;
+  _tmpDstRt.h = 32;
+  SDL_Rect _tmpSrcRt;
+  _tmpSrcRt.x = (_index % 6) * 8;
+  _tmpSrcRt.y = (_index / 6) * 8;
+  _tmpSrcRt.w = 8;
+  _tmpSrcRt.h = 8;
+
+  SDL_RenderCopy(pRenderer, pTex, &_tmpSrcRt, &_tmpDstRt);
+} 
