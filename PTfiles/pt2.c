83fd59bf8b507f5e72b5746eb4eccb8d0bfbd972
KINOTZ <sneak7581@wku.ac.kr>
2020-08-09 14:16:27 +0900

    makefile, readme files

--- PTfiles/pt2.c
+++ PTfiles/pt2.c
@@ -7,10 +7,10 @@
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    //SDL INIT 
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    //SDL INIT
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
         printf("Error initializing : %s \n", SDL_GetError);
         return 1;
@@ -22,13 +22,13 @@ int main(int argc,char *argv[])
 
     // Create a Window
     SDL_Window *gWindow = SDL_CreateWindow("GAME",
-    SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED,
-    WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+                                           SDL_WINDOWPOS_CENTERED,
+                                           SDL_WINDOWPOS_CENTERED,
+                                           WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 
     // trigger the program that controls Graphic hardware and set flags
     Uint32 render_flgas = SDL_RENDERER_ACCELERATED;
-    
+
     // create a renderer to render img
     SDL_Renderer *gRenderer = SDL_CreateRenderer(gWindow, -1, render_flgas);
 
@@ -38,27 +38,28 @@ int main(int argc,char *argv[])
 
     // Print Msg if loading IMG files Fail
     if (!surface)
-    {   
+    {
         printf("Error MSG : %s \n", IMG_GetError());
         return 1;
     }
 
     // load Img to Graphic hardware memory.
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(gRenderer, surface);;
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(gRenderer, surface);
+    ;
     // tex = SDL_CreateTextureFromSurface(gRenderer, surface);
 
     // clear main-memory
     SDL_FreeSurface(surface);
 
     SDL_bool bLoop = SDL_FALSE;
-    while(bLoop)
+    while (bLoop)
     {
         //cears the screen
         SDL_RenderClear(gRenderer);
 
-        for(int iy = 0; iy < 8; iy++)
+        for (int iy = 0; iy < 8; iy++)
         {
-            for(int ix = 0; ix < 10; ix++)
+            for (int ix = 0; ix < 10; ix++)
             {
                 SDL_Rect src = {16 * 6, 16 * 1, 16, 16};
                 SDL_Rect dst = {ix * 64, iy * 64, 64, 64};
@@ -78,4 +79,7 @@ int main(int argc,char *argv[])
             SDL_RenderCopy(gRenderer, tex, &src, &dst);
         }
     }
+    SDL_DestroyWindow(gWindow);
+
+    return 1;
 }
