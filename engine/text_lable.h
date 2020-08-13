2003c6b6a38c4328bfe248a4d6449f6bce6129b5
KINOTZ <sneak7581@wku.ac.kr>
2020-08-13 16:27:18 +0900

    text_lable.h fix

--- engine/text_lable.h
+++ engine/text_lable.h
@@ -6,8 +6,8 @@ typedef struct text_lable
 Uint16 m_nType; //오브잭트 구분하기 위한 형식 식별자
 SDL_bool m_bVisible;
 Uint16 m_nID;
-void (*m_fpDestory)(struct text_lable *pObj);
-void (*m_fpRender)(struct text_lable *pObj, SDL_Renderer *pRender);
+void (*m_fpDestory)(void *pObj);
+void (*m_fpRender)(void *pObj, SDL_Renderer *pRender);
 
 TTF_Font *m_pFont;
 SDL_Texture *m_pLableTxture;
