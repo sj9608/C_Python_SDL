d221bf9f4ce4336186d69957e85ea4a50bed5674
KINOTZ <sneak7581@wku.ac.kr>
2020-08-18 15:30:47 +0900

    before rendercopyEx

--- Week04/day17/ex02-2.c
+++ Week04/day17/ex02-2.c
@@ -5,74 +5,104 @@ int g_select_SheetIndex = 0;
 
 int main(int argc, char *argv[])
 {
-  tDE_S_Core *pEngineCore = tDE_setup_1("example2", 640, 480, 0);
+    tDE_S_Core *pEngineCore = tDE_setup_1("example2", 640, 480, 0);
 
-  SDL_bool bLoop = SDL_TRUE;
-
-  {
-    int nCount = 0;
-    static char szBuf[256];
-    FILE *fp = fopen("../../res/tanks/sheet_tanks.xml", "rt");
-    while (fgets(szBuf, sizeof(szBuf), fp))
-    {
-      tDE_S_SheetData _tmp;
-      if (tDE_util_parseSheet(szBuf, &_tmp))
-      {
-        g_sheetDataAssets[nCount++] = _tmp;
-        // printf("%32s%4d%4d%4d%4d\n", data.szName, data.x, data.y, data.w, data.h);
-      }
-    }
-
-    fclose(fp);
-  }
-
-  SDL_Texture *pTanksTex = tDE_util_loadTexture(pEngineCore, "../../res/tanks/sheet_tanks.png");
-
-  while (bLoop)
-  {
-
-    SDL_SetRenderDrawColor(pEngineCore->m_pRender, 0, 0, 0, 0xff);
-    SDL_RenderClear(pEngineCore->m_pRender);
+    SDL_bool bLoop = SDL_TRUE;
 
     {
-      Uint16 sheet_index = g_select_SheetIndex;
-      SDL_Rect _destRt = {320, 240,
-                          g_sheetDataAssets[sheet_index].m_area.w,
-                          g_sheetDataAssets[sheet_index].m_area.h};
-      SDL_RenderCopy(pEngineCore->m_pRender, pTanksTex,
-                     &g_sheetDataAssets[sheet_index].m_area, &_destRt);
-    }
-
-    SDL_RenderPresent(pEngineCore->m_pRender);
-
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-      switch (event.type)
-      {
-      case SDL_KEYDOWN:
-        printf("%4d\r",event.key.keysym.scancode);
-        if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+        int nCount = 0;
+        static char szBuf[256];
+        FILE *fp = fopen("../../res/tanks/sheet_tanks.xml", "rt");
+        while (fgets(szBuf, sizeof(szBuf), fp))
         {
-          if (g_select_SheetIndex > 0)
-          {
-            g_select_SheetIndex--;
-          }
+            tDE_S_SheetData _tmp;
+            if (tDE_util_parseSheet(szBuf, &_tmp))
+            {
+                g_sheetDataAssets[nCount++] = _tmp;
+                // printf("%32s%4d%4d%4d%4d\n", data.szName, data.x, data.y, data.w, data.h);
+            }
         }
-        else if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
-        {
-          g_select_SheetIndex++;
-        }
-        break;
-      case SDL_QUIT:
-        bLoop = SDL_FALSE;
-        break;
-      }
+
+        fclose(fp);
     }
-  }
 
-  SDL_DestroyTexture(pTanksTex);
+    SDL_Texture *pTanksTex = tDE_util_loadTexture(pEngineCore, "../../res/tanks/sheet_tanks.png");
 
-  tDE_closeCore(pEngineCore);
-  return 0;
+    while (bLoop)
+    {
+
+        SDL_SetRenderDrawColor(pEngineCore->m_pRender, 0, 0, 0, 0xff);
+        SDL_RenderClear(pEngineCore->m_pRender);
+
+        {
+            SDL_SetRenderDrawColor(pEngineCore->m_pRender, 0x00, 0xff, 0x00, 0xff);
+            SDL_RenderDrawLine(pEngineCore->m_pRender, pEngineCore->m_ScreenWidth / 2, 0, pEngineCore->m_ScreenWidth / 2, pEngineCore->m_ScreenHeight);
+            SDL_RenderDrawLine(pEngineCore->m_pRender, pEngineCore->m_ScreenWidth / 2, 0, pEngineCore->m_ScreenWidth / 2, pEngineCore->m_ScreenHeight);
+        }
+
+        {
+            Uint16 sheet_index = g_select_SheetIndex;
+            SDL_Rect _destRt = {320 - g_sheetDataAssets[sheet_index].m_area.w / 2, // 물체 중심 가운데로 맞추기
+                                240 - g_sheetDataAssets[sheet_index].m_area.h/2,
+                                g_sheetDataAssets[sheet_index].m_area.w,
+                                g_sheetDataAssets[sheet_index].m_area.h};
+            SDL_RenderCopy(pEngineCore->m_pRender, pTanksTex,
+                           &g_sheetDataAssets[sheet_index].m_area, &_destRt);
+        }
+
+        {
+            SDL_Color _whiteColor = {0xff, 0xff, 0xff, 0xff};
+
+            // g_sheetDataAssets[g_select_SheetIndex] szName
+            tDE_S_SheetData *pSheet = &g_sheetDataAssets[g_select_SheetIndex];
+            char szInfo[256];
+            sprintf(szInfo, "%s,%4d%4d%4d%4d",
+                    pSheet->szName,
+                    pSheet->m_area.x,
+                    pSheet->m_area.y,
+                    pSheet->m_area.w,
+                    pSheet->m_area.h);
+
+            SDL_Surface *textSurface = TTF_RenderText_Solid(pEngineCore->m_pDefaultFont, g_sheetDataAssets[g_select_SheetIndex].szName, _whiteColor);
+            SDL_Texture *pTex = SDL_CreateTextureFromSurface(pEngineCore->m_pRender, textSurface);
+
+            SDL_Rect _dstRect = {120, 120, textSurface->w, textSurface->h};
+            SDL_RenderCopy(pEngineCore->m_pRender, pTex, NULL, &_dstRect);
+
+            SDL_FreeSurface(textSurface);
+            SDL_DestroyTexture(pTex);
+        }
+
+        SDL_RenderPresent(pEngineCore->m_pRender);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_KEYDOWN:
+                printf("%4d\r", event.key.keysym.scancode);
+                if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+                {
+                    if (g_select_SheetIndex > 0)
+                    {
+                        g_select_SheetIndex--;
+                    }
+                }
+                else if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+                {
+                    g_select_SheetIndex++;
+                }
+                break;
+            case SDL_QUIT:
+                bLoop = SDL_FALSE;
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyTexture(pTanksTex);
+
+    tDE_closeCore(pEngineCore);
+    return 0;
 }
--- engine/core.c
+++ engine/core.c
@@ -19,28 +19,30 @@ tDE_S_Core *tDE_setup_1(const char *szTitle,
                                      SDL_WINDOWPOS_CENTERED,
                                      window_width,
                                      window_height, flags);
+    pCore->m_ScreenWidth = window_width; // 초기화
+    pCore->m_ScreenHeight = window_height;
 
     pCore->m_pRender = SDL_CreateRenderer(pCore->m_pWin, -1, SDL_RENDERER_ACCELERATED);
-    
-    if(!IMG_Init(IMG_INIT_PNG) &IMG_INIT_PNG)
+
+    if (!IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)
     {
-        printf("SDL image dll init Error : %s \n",SDL_GetError());
+        printf("SDL image dll init Error : %s \n", SDL_GetError());
         return NULL;
     }
 
-    if(TTF_Init() == -1)
+    if (TTF_Init() == -1)
     {
-        printf("sdl TTF dll init Error : %s\n",SDL_GetError());
+        printf("sdl TTF dll init Error : %s\n", SDL_GetError());
         return NULL;
     }
-    
-    pCore->m_pDefaultFont = TTF_OpenFont("../../res/nmf.ttf",14);
-    if(!pCore->m_pDefaultFont)
+
+    pCore->m_pDefaultFont = TTF_OpenFont("../../res/nmf.ttf", 14);
+    if (!pCore->m_pDefaultFont)
     {
-        printf("load default font Error : %s\n",SDL_GetError());
+        printf("load default font Error : %s\n", SDL_GetError());
     }
     printf("init Module ok\n");
-    printf("engine start at %8d\n",time(NULL));
+    printf("engine start at %8d\n", time(NULL));
     srand(time(NULL));
 
     return pCore;
@@ -58,9 +60,9 @@ SDL_Texture *tDE_util_loadTexture(tDE_S_Core *pCore, const char *filename)
     SDL_Surface *surface;
     surface = IMG_Load(filename);
 
-    if(surface == NULL)
+    if (surface == NULL)
     {
-        printf("file load error : %s\n",SDL_GetError());
+        printf("file load error : %s\n", SDL_GetError());
         return NULL;
     }
     else
@@ -71,66 +73,65 @@ SDL_Texture *tDE_util_loadTexture(tDE_S_Core *pCore, const char *filename)
     SDL_FreeSurface(surface);
 
     return pTex;
-    
 }
 
 SDL_bool tDE_util_parseSheet(char *szStr, tDE_S_SheetData *pData)
 {
-      char *szToken = strtok(szStr, " ");
-  if (!strcmp(szToken, "\t<SubTexture"))
-  {
-    static char _szBuf[5][256];
-    //name
-    szToken = strtok(NULL, " ");
-    strcpy(_szBuf[0], szToken);
-    //x
-    szToken = strtok(NULL, " ");
-    strcpy(_szBuf[1], szToken);
-    //y
-    szToken = strtok(NULL, " ");
-    strcpy(_szBuf[2], szToken);
-    //w
-    szToken = strtok(NULL, " ");
-    strcpy(_szBuf[3], szToken);
-    //h
-    szToken = strtok(NULL, " ");
-    strcpy(_szBuf[4], szToken);
+    char *szToken = strtok(szStr, " ");
+    if (!strcmp(szToken, "\t<SubTexture"))
+    {
+        static char _szBuf[5][256];
+        //name
+        szToken = strtok(NULL, " ");
+        strcpy(_szBuf[0], szToken);
+        //x
+        szToken = strtok(NULL, " ");
+        strcpy(_szBuf[1], szToken);
+        //y
+        szToken = strtok(NULL, " ");
+        strcpy(_szBuf[2], szToken);
+        //w
+        szToken = strtok(NULL, " ");
+        strcpy(_szBuf[3], szToken);
+        //h
+        szToken = strtok(NULL, " ");
+        strcpy(_szBuf[4], szToken);
 
-    //name 파싱
-    szToken = strtok(_szBuf[0], "=");
-    szToken = strtok(NULL, ".");
-    strcpy(_szBuf[0], szToken + 1);
-    //printf("%s\n", _szBuf[0]);
-    strcpy(pData->szName,_szBuf[0]);
+        //name 파싱
+        szToken = strtok(_szBuf[0], "=");
+        szToken = strtok(NULL, ".");
+        strcpy(_szBuf[0], szToken + 1);
+        //printf("%s\n", _szBuf[0]);
+        strcpy(pData->szName, _szBuf[0]);
 
-    //x 파싱
-    szToken = strtok(_szBuf[1], "\"");
-    szToken = strtok(NULL, "\"");
-    // strcpy(_szBuf[1],szToken+1);
-    // printf("x:%d\n", atoi(szToken));
-    pData->m_area.x = atoi(szToken);
+        //x 파싱
+        szToken = strtok(_szBuf[1], "\"");
+        szToken = strtok(NULL, "\"");
+        // strcpy(_szBuf[1],szToken+1);
+        // printf("x:%d\n", atoi(szToken));
+        pData->m_area.x = atoi(szToken);
 
-    //y 파싱
-    szToken = strtok(_szBuf[2], "\"");
-    szToken = strtok(NULL, "\"");
-    // strcpy(_szBuf[1],szToken+1);
-    // printf("y:%d\n", atoi(szToken));
-    pData->m_area.y = atoi(szToken);
+        //y 파싱
+        szToken = strtok(_szBuf[2], "\"");
+        szToken = strtok(NULL, "\"");
+        // strcpy(_szBuf[1],szToken+1);
+        // printf("y:%d\n", atoi(szToken));
+        pData->m_area.y = atoi(szToken);
 
-    //w
-    szToken = strtok(_szBuf[3], "\"");
-    szToken = strtok(NULL, "\"");
-    // strcpy(_szBuf[1],szToken+1);
-    // printf("w:%d\n", atoi(szToken));
-    pData->m_area.w = atoi(szToken);
+        //w
+        szToken = strtok(_szBuf[3], "\"");
+        szToken = strtok(NULL, "\"");
+        // strcpy(_szBuf[1],szToken+1);
+        // printf("w:%d\n", atoi(szToken));
+        pData->m_area.w = atoi(szToken);
 
-    //h
-    szToken = strtok(_szBuf[4], "\"");
-    szToken = strtok(NULL, "\"");
-    // strcpy(_szBuf[1],szToken+1);
-    // printf("h:%d\n", atoi(szToken));
-    pData->m_area.h = atoi(szToken);
-    return 1;
-  }
-  return 0;
+        //h
+        szToken = strtok(_szBuf[4], "\"");
+        szToken = strtok(NULL, "\"");
+        // strcpy(_szBuf[1],szToken+1);
+        // printf("h:%d\n", atoi(szToken));
+        pData->m_area.h = atoi(szToken);
+        return 1;
+    }
+    return 0;
 }
